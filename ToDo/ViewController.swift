c156767b996240b9e4babdb8f17a9b91c8e51b60
fadeltd <fadeltd@hotmail.com>
2019-02-27 14:34:43 +0700

    add hello

--- ToDo/ViewController.swift
+++ ToDo/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("This is main page")
+        print("Hello, this is main page")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
